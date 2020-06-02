a3049014cf499dea2cdf4a412179dfbf3bc3f20a
ale97dro <alex24br@yahoo.it>
2020-06-02 19:20:23 +0200

    copyright header

--- src/main/java/command/ShowCommand.java
+++ src/main/java/command/ShowCommand.java
@@ -1,6 +1,13 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) 2020 Alessandro Bianchi
+ *  *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *  * github.com/ale97dro/NWCenter
+ *  *--------------------------------------------------------------------------------------------
+ */
+
 package command;
 
-import console.ConsolePrinter;
 import formatter.Formatter;
 import formatter.ShowFormatter;
 import model.DBContainer;
--- src/main/java/formatter/ExitFormatter.java
+++ src/main/java/formatter/ExitFormatter.java
@@ -1,3 +1,11 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) 2020 Alessandro Bianchi
+ *  *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *  * github.com/ale97dro/NWCenter
+ *  *--------------------------------------------------------------------------------------------
+ */
+
 package formatter;
 
 import console.ConsolePrinter;
